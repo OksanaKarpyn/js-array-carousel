48dee916cb381ec1df9a178ee00c086ff9ce5499
OksanaKarpyn <93349703+OksanaKarpyn@users.noreply.github.com>
2023-03-28 21:22:53 +0200

    add stile

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,15 @@ const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 
 let arrayImg = [
-    '<img class="item  first" src="./assets/img/02.webp" alt="foto" width="100%">',
-    '<img class="item active" src="./assets/img/04.webp" alt="foto" width="100%">',
-    '<img class="item" src="./assets/img/03.webp" alt="foto" width="100%">',
-    '<img class="item" src="./assets/img/01.webp" alt="foto" width="100%">',
-    '<img class="item last" src="./assets/img/05.webp" alt="foto" width="100%">',
+    '<img class="item active first" src="./assets/img/04.webp" alt="foto" width="100%">',
+    '<img class="item opacita" src="./assets/img/02.webp" alt="foto" width="100%">',
+    '<img class="item opacita" src="./assets/img/03.webp" alt="foto" width="100%">',
+    '<img class="item opacita" src="./assets/img/01.webp" alt="foto" width="100%">',
+    '<img class="item last opacita" src="./assets/img/05.webp" alt="foto" width="100%">',
 ];
 console.log(arrayImg);
 
 let boxImg = document.querySelector('.items');
-
 for (let i = 0; i < arrayImg.length; i++) {
     boxImg.innerHTML += arrayImg[i];
 
@@ -22,10 +21,10 @@ for (let i = 0; i < arrayImg.length; i++) {
 prev.addEventListener('click', function () {
     let imgActive = document.querySelector('.item.active');
     // mi aggancio ad elem sucessivo cioe fratello ,creo un altra var
-    boxImg = imgActive.nextElementSibling
+    boxImg = imgActive.previousElementSibling
     // ciclo del infinito giro
-    if (imgActive.classList.contains('last')) { //quando active sara al ultimo elemento
-        boxImg = document.querySelector('.first'); //torna al inizio e cominci da capo
+    if (imgActive.classList.contains('first')) { //quando active sara al ultimo elemento
+        boxImg = document.querySelector('.last'); //torna al inizio e cominci da capo
     }
     // rimovo dal attuale active
     imgActive.classList.remove('active');
